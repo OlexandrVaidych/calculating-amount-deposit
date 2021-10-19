b38d05bcbc8581d79948be94cada7362678d4150
unknown <10lexandr.vaidych@gmail.com>
2021-10-19 11:46:45 +0300

    Added project

---
+++ CalculatingAmountDeposit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatingAmountDeposit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
